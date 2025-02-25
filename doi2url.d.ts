b1f97b5989d36ee319a0e70d63a89e44600a6f2d
Stephane Pericat <shortwavlabs@Mac.attlocal.net>
2025-02-25 17:25:06 -0600

    add declaration files

---
+++ doi2url.d.ts
@@ -0,0 +1,9 @@
+/**
+ * doi2url
+ * returns information about a DOI
+ * ex: doi2url('https://doi.org/10.1038/s41591-024-03173-6')
+ * @param url string
+ * @returns string
+ */
+export declare function doi2url(url: string): Promise<string | null>;
+//# sourceMappingURL=doi2url.d.ts.map
